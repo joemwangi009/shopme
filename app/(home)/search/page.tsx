6c9a6ee129fc4cc72f172b8cf2dbddd013f3f516
joemwangi009 <joemwangi009@gmail.com>
2025-08-14 23:45:11 +0300

    fix: Remove unused parameter from onPageChange to resolve final ESLint error

--- app/(home)/search/page.tsx
+++ app/(home)/search/page.tsx
@@ -100,7 +100,7 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
               loading={false}
               currentPage={currentPage}
               totalPages={totalPages}
-              onPageChange={(_page) => {
+              onPageChange={() => {
                 // This will be handled by the ProductSidebar component
                 // or we can implement client-side navigation here
               }}
